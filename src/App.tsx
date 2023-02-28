9c507fb8b83eb936dee7f112623eee6956c1f1ae
Yining <yiningcherry@gmail.com>
2023-02-28 13:43:51 +1100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {`test`}
+        {`test test`}
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
