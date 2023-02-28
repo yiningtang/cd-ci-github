0ef530bc9938a40635d69a3d5d6092624ed391c0
Yining <yiningcherry@gmail.com>
2023-02-28 12:14:49 +1100

    Add test text on the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {`test`}
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
