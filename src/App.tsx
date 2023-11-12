b62d08a6433b701bf284d9b19b236b4f0510e714
yiningtang <yiningchery@gmail.com>
2023-11-12 15:04:02 +1100

    feat: react redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { reactAppEnv, nodeEnv}  from './constant'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {`test test`}
+        {`test test`} {`this is  react app env ${reactAppEnv} node env is ${nodeEnv}`}
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
---
+++ src/constant.ts
@@ -0,0 +1,2 @@
+export const nodeEnv = process.env.NODE_ENV;
+export const reactAppEnv = process.env.REACT_APP_ENV;
