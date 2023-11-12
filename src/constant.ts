d3d356ee696a493cd2c90bb1dd5057283416aa33
yiningtang <yiningchery@gmail.com>
2023-11-12 16:25:06 +1100

    fix yaml

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
-export const nodeEnv = process?.env?.NODE_ENV || '';
-export const reactAppEnv = process?.env?.REACT_APP_ENV || ''
+export const nodeEnv = process.env?.NODE_ENV || '';
+export const reactAppEnv = process.env?.REACT_APP_ENV || ''
